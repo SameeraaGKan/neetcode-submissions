46b4b7484501672c8b01956960b586f91bd9f7d2
SameeraaGKan <143641971+SameeraaGKan@users.noreply.github.com>
2026-07-19 13:28:55 -0500

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # so first we's use a hashmap to register the frequancies of the nums
+        count = {}
+
+        for num in nums:
+            if num in count:
+                count[num] += 1
+            else:
+                count[num] = 1
+        
+        # soo turns out there's a better and efficient way to do this 
+        # instead of sorting and then slicing - we would be using a heap  
+        # sorted_items = sorted(count.items(), key=lambda x:x[1], reverse=True)
+        sorted_items = heapq.nlargest(k, count.items(), key = lambda x: x[1])
+        top_k = sorted_items[:k]
+        
+        return [x[0] for x in top_k]
